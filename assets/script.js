84b4f22194d9bad17c9681f5b4d2fc49230736e9
heyemmibee <heyemmibee@yahoo.com>
2019-09-09 19:33:40 -0400

    transform completed

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,15 @@ let secP = sec*360/60;
 let minP = (min*360/60)+(sec*(360/60)/60);
 let hourP = (hr*360/12)+(min*(360/60)/12);
 
-HOURHAND.style.transform = "rotate(" + hourP + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minP + "deg)";
-SECONDHAND.style.transform = "rotate(" + secP + "deg)";
+function ticktock () {
+
+    secP = secP+6;
+    minP = minP+(6/60);
+    hourP = hourP+(30/360);
+
+    HOURHAND.style.transform = "rotate(" + hourP + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minP + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secP + "deg)";
+}
+
+var interval = setInterval(ticktock, 1000);
